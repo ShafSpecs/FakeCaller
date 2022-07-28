cd2af840f2b8cff39e605713385962207f327de0
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-28 14:00:21 +0100

    Created contact service interface along with the module

--- src/africa/semicolon/fakeCaller/services/ContactService.java
+++ src/africa/semicolon/fakeCaller/services/ContactService.java
@@ -1,10 +1,29 @@
 package africa.semicolon.fakeCaller.services;
 
 import africa.semicolon.fakeCaller.data.models.Contact;
+import africa.semicolon.fakeCaller.data.repositories.ContactRepository;
+
+import java.util.List;
 
 public class ContactService implements iContactService {
+    private ContactRepository repo;
+
+    public ContactService () {
+        this.repo = new ContactRepository();
+    }
+
+    public ContactService (ContactRepository repo) {
+        this.repo = repo;
+    }
+
     @Override
     public Contact addNewContact(Contact contact) {
+        repo.save(contact);
         return contact;
+    }
+
+    @Override
+    public List<Contact> getAllContacts() {
+        return repo.findAll();
     }
 }
--- src/africa/semicolon/fakeCaller/services/iContactService.java
+++ src/africa/semicolon/fakeCaller/services/iContactService.java
@@ -2,6 +2,9 @@ package africa.semicolon.fakeCaller.services;
 
 import africa.semicolon.fakeCaller.data.models.Contact;
 
+import java.util.List;
+
 public interface iContactService {
     Contact addNewContact(Contact contact);
+    List<Contact> getAllContacts();
 }
